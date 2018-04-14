2178d35569083e915aebf7c939491df17a0f389d
Blake La Pierre <blakelapierre@gmail.com>
2018-04-14 02:18:20 -0400

    dupe

--- www/src/pools.ts
+++ www/src/pools.ts
@@ -1,5 +1,4 @@
 export default [
-'https://cryptoknight.cc/rpc/graft/live_stats',
   'https://cryptoknight.cc/rpc/graft/live_stats',
   'https://graftmine.com/api/stats',
   'http://graft-pool-mainnet-mining1-9dfb326130b73cf5.elb.eu-central-1.amazonaws.com:8117/live_stats',
